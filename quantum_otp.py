bafe5ad56e06e54abec42b86e822547044652a42
01010011IT <113771618+01010011IT@users.noreply.github.com>
2023-12-14 12:06:07 +0300

    Update quantum_otp.py

--- quantum_otp.py
+++ quantum_otp.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 13 16:22:46 2023
 
-@author: Sarah
-"""
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister, execute, Aer
+from collections import Counter
 
 n=16
 otpArray = []
@@ -14,17 +10,19 @@ def generate_otp ():
     circuit.h(range(n))
     circuit.measure_all()
     backend = Aer.get_backend("qasm_simulator")
-    job = execute(circuit, backend, shots=1,  timeout=60)
+    job = execute(circuit, backend, shots=1000)
     result = job.result()
     counts = result.get_counts(circuit)
-    output = list(counts)[0][0:17]
+    output = list(counts)[:-1]
+    every_output= [str(num)[:17] for num in output]
     circuit.reset(range(n))
+    print(every_output)
     circuit.draw('mpl')
-    return circuit, output
+    return circuit,  every_output
 
 circuit, output = generate_otp()
 
-
+every_otp= []
 def conver_to_decimal(binary_string):
     binary_string = ''.join(filter(lambda x: x in '01', binary_string))
     chunks = [binary_string[i:i+4] for i in range(0, len(binary_string), 4)]
@@ -35,28 +33,33 @@ def conver_to_decimal(binary_string):
         except ValueError:
             print(f"Error: '{chunk}' is not a valid 4-bit binary chunk.")
     string_list = list(map(str, decimal_values))
-    # Join the string representations without a separator
     result_string = ''.join(string_list)
     otp_Result = result_string[:4]
+    every_otp.append(otp_Result)
     return otp_Result 
     
-otp_Result  = conver_to_decimal(output)
+for i in output:
+    conver_to_decimal(i)
 
-if otp_Result in otpArray: 
-   circuit.reset(range(n))
-   circuit, output = generate_otp() 
-   otp_Result  = conver_to_decimal(output)
+print("every_otp", every_otp)
 
 
-def save_output_to_file(output):
-    with open('otpDataBases.txt', 'a') as file:
-        file.write(output + '\n')
+element_counts = Counter(every_otp)
 
-new_output = otp_Result 
-save_output_to_file(otp_Result )
+for element, count in element_counts.items():
+    if count > 1:
+        print(f"Element {element} repeats {count} times.")
+    
+def save_output_to_file(every_otp):
+    with open('DataBase.txt', 'a') as file:
+        for value in every_otp:
+            file.write(value + '\n')
+
+new_output = every_otp
+save_output_to_file(new_output)
 
 
-def read_values_from_file(file_path='.\otpDataBases.txt'):
+def read_values_from_file(file_path='.\DataBase.txt'):
     with open(file_path, 'r') as file:
         values = file.readlines()
     values = [value.strip() for value in values]
@@ -64,7 +67,7 @@ def read_values_from_file(file_path='.\otpDataBases.txt'):
 
 
 # Read the values from the file
-file_path = 'otpDataBases.txt'
+file_path = 'DataBase.txt'
 saved_values = read_values_from_file(file_path)
 
 # Use the saved values
